f52ad9264680d5afcb3d5360d2949583b4359ca0
Nordy Vlasman <nordy@sigbar.nl>
2021-12-23 23:37:30 +0100

    [FEATURE] base entity

---
+++ src/core/base.entity.ts
@@ -0,0 +1,27 @@
+import { IBaseEntityModel } from 'src/interfaces';
+import {
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export abstract class Model {
+  constructor(input?: any) {
+    if (input) {
+      for (const [key, value] of Object.entries(input)) {
+        (this as any)[key] = value;
+      }
+    }
+  }
+}
+
+export abstract class BaseEntity extends Model implements IBaseEntityModel {
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @UpdateDateColumn()
+  updatedAt?: Date;
+}
