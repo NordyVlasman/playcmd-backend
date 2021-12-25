90af990d8826932919bd15402dc4982b26d65997
Nordy Vlasman <nordy@sigbar.nl>
2021-12-26 00:54:11 +0100

    [FEATURE] Added columns.

--- src/community/community.entity.ts
+++ src/community/community.entity.ts
@@ -1,16 +1,31 @@
 import { BaseEntity } from 'src/core/base.entity';
 import { ICommunity, IUser } from 'src/interfaces';
-import { Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 
 @Entity({ name: 'communities' })
 export class Community extends BaseEntity implements ICommunity {
+  @Column()
   title: string;
+
+  @Column()
   slug: string;
+
   owner?: IUser;
+
+  @Column()
   ownerId: number;
+
+  @Column()
   smallTitle?: string;
+
+  @Column()
   communityAvatar?: string;
+
+  @Column()
   summary?: string;
+
   createdBy?: IUser;
+
+  @Column()
   createdById: number;
 }
