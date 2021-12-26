f9febf800a9c18bc9f0ee1fc3e55e9522b318e82
Nordy Vlasman <nordy@sigbar.nl>
2021-12-26 22:41:57 +0100

    [FEATURE] Import entities in module

--- src/social-link/social-link.module.ts
+++ src/social-link/social-link.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SocialLink } from './social-link.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([SocialLink])],
+})
 export class SocialLinkModule {}
