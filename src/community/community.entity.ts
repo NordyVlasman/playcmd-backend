ebedec15e5c93f43cc9fa87c10de851001c86676
Nordy Vlasman <nordy@sigbar.nl>
2021-12-27 00:40:17 +0100

    [FIX] Wip

--- src/community/community.entity.ts
+++ src/community/community.entity.ts
@@ -1,13 +1,6 @@
 import { BaseEntity } from 'src/core/base.entity';
 import { ICommunity, IUser } from 'src/interfaces';
-import {
-  Column,
-  Entity,
-  JoinColumn,
-  ManyToOne,
-  OneToOne,
-  RelationId,
-} from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
 import { User } from 'src/user/user.entity';
 
 @Entity({ name: 'communities' })
@@ -18,10 +11,8 @@ export class Community extends BaseEntity implements ICommunity {
   @Column()
   slug: string;
 
-  @OneToOne(() => User, {
-    nullable: true,
-    onDelete: 'SET NULL',
-  })
+  @OneToMany(() => User, (user) => user.ownedCommunities)
+  @JoinColumn()
   owner?: IUser;
 
   @Column()
