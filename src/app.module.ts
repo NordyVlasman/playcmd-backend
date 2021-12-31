a5dbc4155f61377a80e7dab690453e537c5b1eab
Nordy Vlasman <nordyvla@gmail.com>
2021-12-31 13:31:53 +0100

    [FEATURE] Auth service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { UserModule } from './user/user.module';
 import { CommunityModule } from './community/community.module';
 import { RoleModule } from './role/role.module';
 import { SocialLinkModule } from './social-link/social-link.module';
+import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [UserModule, CommunityModule, RoleModule, SocialLinkModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
