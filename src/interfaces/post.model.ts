9670386275b42ab3de7792aac5db3890cc8bdf32
Nordy Vlasman <nordyvla@gmail.com>
2022-01-01 13:12:53 +0100

    [FEATURE] More post content

--- src/interfaces/post.model.ts
+++ src/interfaces/post.model.ts
@@ -1,8 +1,12 @@
-import { IBaseEntityModel } from '.';
+/* eslint-disable prettier/prettier */
+import { IBaseEntityModel, IUser } from '.';
 
 export interface IPost extends IBaseEntityModel {
   title?: string;
   slug?: string;
 
+  body?: string;
+  
+  author?: IUser;
   publishedAt?: Date;
 }
